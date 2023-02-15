7c97e3250d6980ad70886a90172d4580b54e83d1
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:26:17 -0300

    Delete ex002.py

--- ex002.py
+++
@@ -1,8 +0,0 @@
-print('====== EXERCÍCIO 002 ======')
-#Exercício Python 2: Faça um programa que leia o nome de uma pessoa
-#e mostre uma mensagem de boas-vindas.
-
-nome = input('Digite seu nome: ')
-print('É u prazer te conhecer, ', nome)
-print('É u prazer te conhecer, {}.'.format(nome))
-print('É u prazer te conhecer, ', input('Digite seu nome: '))

925332106d0ad4a84ee0924df234ebdf4f520974
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:28:40 -0300

    Delete ex014.py

--- ex014.py
+++
@@ -1,8 +0,0 @@
-print('====== EXERCÍCIO 14 ======')
-#Exercício Python 14: Escreva um programa que converta
-# uma temperatura digitando em graus Celsius
-# e converta para graus Fahrenheit.
-
-graus = float(input('Digite uma temperatura em ºC: '))
-fahrenheit = (graus * 1.8) + 32
-print(f'A temperatura de {graus:.1f}ºC corresponde a {fahrenheit:.1f}ºF!')

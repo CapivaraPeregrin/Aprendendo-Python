fb2e32d32fd26b0c3bef5ffbcca21b2375bd2222
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:27:28 -0300

    Delete ex008.py

--- ex008.py
+++
@@ -1,7 +0,0 @@
-print('====== EXERCÍCIO 08 ======')
-#Exercício Python 8: Escreva um programa que
-# leia um valor em metros e o exiba convertido
-# em centímetros e milímetros.
-
-metro = float(input('Digite um valor em metros: '))
-print('O valor corresponde a {:.0f}cm \nE a {:.0f}mm'.format(metro*100, metro*1000))

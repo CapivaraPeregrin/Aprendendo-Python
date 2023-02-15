3039b7402b50920e69d85aff1759ce26dcf07692
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:28:27 -0300

    Delete ex012.py

--- ex012.py
+++
@@ -1,8 +0,0 @@
-print('====== EXERCÍCIO 12 ======')
-#Exercício Python 12: Faça um algoritmo
-# que leia o preço de um produto e mostre
-# seu novo preço, com 5% de desconto.
-
-preco = float(input('Digite um valor: '))
-novopreco = preco*0.95
-print(f'O preço do produto com desconto de 5% é R${novopreco:.2f}')

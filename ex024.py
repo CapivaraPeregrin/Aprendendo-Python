e6a6daddfee00e3e1725c03e4a5b4bb5c366705b
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:32:23 -0300

    Delete ex024.py

--- ex024.py
+++
@@ -1,9 +0,0 @@
-print('====== EXERCÍCIO 24 ======')
-
-#Exercício Python 24: Crie um programa
-# que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
-
-cidade = str(input('Em que cidade você nasceu? ')).strip().split()[0].upper()
-print(cidade == 'SANTO')
-
-

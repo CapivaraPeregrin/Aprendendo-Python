fb574571eacc80bbb6053e7e16b3d8a242098f44
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:32:29 -0300

    Delete ex025.py

--- ex025.py
+++
@@ -1,7 +0,0 @@
-print('====== EXERCÍCIO 25 ======')
-
-#Exercício Python 25: Crie um programa que leia o
-# nome de uma pessoa e diga se ela tem “SILVA” no nome.
-
-nome = str(input('Qual é o seu nome completo? ')).upper().strip().split()
-print('Seu nome tem Silva? {}'.format('SILVA' in nome))

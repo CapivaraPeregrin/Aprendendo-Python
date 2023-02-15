7e2414babb669bca69f62ab76f6aa3ce75e3104c
CapivaraPeregrin <100213835+CapivaraPeregrin@users.noreply.github.com>
2023-02-15 09:28:34 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,8 +0,0 @@
-print('====== EXERCÍCIO 13 ======')
-#Exercício Python 13: Faça um algoritmo
-# que leia o salário de um funcionário e mostre
-# seu novo salário, com 15% de aumento.
-
-salario = float(input('Digite o salário: '))
-novosalario = salario*1.15
-print(f'O salário com 15% de aumento é R${novosalario:.2f}')
